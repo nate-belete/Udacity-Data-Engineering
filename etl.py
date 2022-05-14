61ab59073b93be585578b0b0c9e3d2f241a6bdb2
natebelete1 <natebelete@gmail.com>
2022-05-14 09:38:37 -0700

    edits to etl and sql_queries py files

--- etl.py
+++ etl.py
@@ -4,40 +4,50 @@ import psycopg2
 import pandas as pd
 from sql_queries import *
 
+def get_files(filepath):
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+    
+    return all_files
 
 def process_song_file(cur, filepath):
-    # open song file
-    df = 
+    # open log file
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0].tolist()
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page']=='NextSong'].copy()
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df['ts'], unit='ms')
+    df['ts'] = t
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = [t, t.dt.hour, t.dt.day, t.dt.weekofyear, t.dt.month, t.dt.year, t.dt.weekday]
+    column_labels = ('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday')
+    time_df = pd.DataFrame.from_dict(dict(zip(column_labels, time_data)))
+
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -56,7 +66,8 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data =  (index, row.ts, row.userId, row.level, songid, artistid, row.sessionId,
+                     row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
--- sql_queries.py
+++ sql_queries.py
@@ -1,52 +1,123 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS fct_songplays"
+user_table_drop = "DROP TABLE IF EXISTS dim_users"
+song_table_drop = "DROP TABLE IF EXISTS dim_songs"
+artist_table_drop = "DROP TABLE IF EXISTS dim_artists"
+time_table_drop = "DROP TABLE IF EXISTS dim_time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
-""")
+                            CREATE TABLE IF NOT EXISTS fct_songplays(
+                                songplay_id VARCHAR PRIMARY KEY, 
+                                start_time TIMESTAMP, 
+                                user_id VARCHAR, 
+                                level VARCHAR, 
+                                song_id VARCHAR, 
+                                artist_id VARCHAR, 
+                                session_id VARCHAR, 
+                                location TEXT, 
+                                user_agent VARCHAR
+                                )
+                        """)
 
 user_table_create = ("""
+                            CREATE TABLE IF NOT EXISTS dim_users(
+                                user_id VARCHAR PRIMARY KEY,
+                                first_name VARCHAR, 
+                                last_name VARCHAR, 
+                                gender VARCHAR, 
+                                level TEXT
+                            )
 """)
 
 song_table_create = ("""
+                            CREATE TABLE IF NOT EXISTS dim_songs(
+                                song_id VARCHAR PRIMARY KEY, 
+                                title VARCHAR, 
+                                artist_id VARCHAR, 
+                                year INT, 
+                                duration FLOAT
+
+                            )
 """)
 
 artist_table_create = ("""
+                            CREATE TABLE IF NOT EXISTS dim_artists(
+                                artist_id VARCHAR PRIMARY KEY,
+                                name VARCHAR, 
+                                location VARCHAR, 
+                                latitude FLOAT, 
+                                longitude FLOAT
+
+                            )
 """)
 
-time_table_create = ("""
+time_table_create = ("""                            
+                            CREATE TABLE IF NOT EXISTS dim_time(
+                                start_time TIMESTAMP PRIMARY KEY, 
+                                hour INT, 
+                                day INT, 
+                                week INT, 
+                                month INT, 
+                                year INT, 
+                                weekday VARCHAR
+
+                            )
+
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO fct_songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
+    ON CONFLICT (songplay_id) DO NOTHING
 """)
 
 user_table_insert = ("""
+    INSERT INTO dim_users (user_id, first_name, last_name, gender, level)
+    VALUES (%s,%s,%s,%s,%s)
+    ON CONFLICT (user_id) DO NOTHING
 """)
 
 song_table_insert = ("""
+    INSERT INTO dim_songs (song_id, title, artist_id, year, duration)
+    VALUES (%s,%s,%s,%s,%s)
+    ON CONFLICT (song_id) DO NOTHING
+    
 """)
 
 artist_table_insert = ("""
+    INSERT INTO dim_artists (artist_id, name, location, latitude, longitude)
+    VALUES (%s,%s,%s,%s,%s)
+    ON CONFLICT (artist_id) DO NOTHING
 """)
 
-
 time_table_insert = ("""
+    INSERT INTO dim_time (start_time, hour, day, week, month, year, weekday)
+    VALUES (%s,%s,%s,%s,%s,%s,%s)
+    ON CONFLICT (start_time) DO NOTHING
 """)
 
 # FIND SONGS
-
 song_select = ("""
+    select
+        tb2.song_id,
+        tb2.artist_id
+        
+    from dim_songs as tb2
+    join dim_artists as tb3
+     on tb2.artist_id = tb3.artist_id
+    where tb2.title = %s
+     and tb3.name = %s
+     and tb2.duration = %s
 """)
 
 # QUERY LISTS
 
 create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+
+print('it worked')
